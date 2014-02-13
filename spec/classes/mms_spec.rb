5b0645cda2a0219f8613c2c1d33b42968e824c4c
Tyler Stroud <tyler@tylerstroud.com>
2014-02-13 11:57:58 -0800

    Revert "Refactored spec a bit, still trying to get around errors on rvm 1.8.7"
    This reverts commit 4eb7b44d75d0c440a78702226ee1f19b672ea83e.

--- spec/classes/mms_spec.rb
+++ spec/classes/mms_spec.rb
@@ -16,8 +16,7 @@ describe 'mms', :type => :class do
   
     it { should contain_exec('download-mms').with(
       :command => 'wget https://mms.mongodb.com/settings/mms-monitoring-agent.tar.gz /tmp',
-      :require => 'Package[wget]'
-    ) }
+    ).that_requires('Package[wget]') }
 
     it { should contain_exec('set-mms-server').with(
       :command => "sed -ie 's|@MMS_SERVER@|https://mms.mongodb.com|' /opt/mongodb/mms/settings.py"
@@ -26,13 +25,10 @@ describe 'mms', :type => :class do
       :command => "sed -ie 's|@API_KEY@|abcdefg|' /opt/mongodb/mms/settings.py"
     ) }
 
-    it { should contain_exec('install-mms').with(
-      :require => '[Exec[download-mms]{:command=>"download-mms"}, File[/opt/mongodb/mms]{:path=>"/opt/mongodb/mms"}]'
-    ) }
+    it { should contain_exec('install-mms').that_requires('File[/opt/mongodb/mms]').that_requires('Exec[download-mms]') }
 
     it { should contain_service('mongodb-mms').with(
       :ensure => 'running',
-      :require => 'File[/etc/init.d/mongodb-mms]'
-    ) }
+    ).that_requires('File[/etc/init.d/mongodb-mms]')}
   end
 end
